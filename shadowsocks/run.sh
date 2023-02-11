55640b4846e2872f3fb5acd1ba38661449621fb7
linbo <yartorias08@163.com>
2023-02-11 19:58:14 +0800

    args

--- shadowsocks/run.sh
+++ shadowsocks/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
 eval $(ps -ef | grep "[0-9] python server\\.py a" | awk '{print "kill "$2}')
-nohup python server.py a >> ssserver.log 2>&1 &
+nohup python3 server.py -p 443 -k password -m chacha20 >> ssserver.log 2>&1 &
 
